191ee00d91f0e5af09476276f9c8f70752d8b62e
Mariano Vazquez <mareanovazquez@protonmail.com>
2024-02-19 10:28:13 -0300

    set up slider fullscreen img individual

---
+++ js/carouselClientes.js
@@ -0,0 +1,35 @@
+const itemClientes = document.getElementsByClassName('itemCliente');
+const sliderContainer = document.getElementById('sliderClientes')
+
+const slideWidth = sliderContainer.offsetWidth / 12; // Ancho de cada slide dividiendo el contenedor en 6
+
+let currentPosition = 0;
+
+function nextSlide() {
+
+
+
+    if (currentPosition > -(slideWidth * 6)) {
+
+        currentPosition -= slideWidth;
+        sliderContainer.style.transform = `translateX(${currentPosition}px)`;
+        sliderContainer.style.transition = "transform 1.4s ease-in-out";
+
+    } else {
+        // Si estás en el último slide, regresa al principio
+        currentPosition = 0;
+
+        sliderContainer.style.transform = `translateX(${currentPosition}px)`;
+        sliderContainer.style.transition = 'transform 0s';
+        sliderContainer.insertBefore(itemClientes[6], itemClientes[0])
+        sliderContainer.insertBefore(itemClientes[7], itemClientes[1])
+        sliderContainer.insertBefore(itemClientes[8], itemClientes[2])
+        sliderContainer.insertBefore(itemClientes[9], itemClientes[3])
+        sliderContainer.insertBefore(itemClientes[10], itemClientes[4])
+        sliderContainer.insertBefore(itemClientes[11], itemClientes[5])
+        // Vuelve a activar la transición después de realizar los cambios
+    }
+}
+
+// Configura el intervalo para cambiar automáticamente los slides cada 3 segundos
+const intervalId = setInterval(nextSlide, 4500);
